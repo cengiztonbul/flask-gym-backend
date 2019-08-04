8fa2c76f441898d60f99cc86f6e6900bf887f382
cengiztonbul <cengiztonbul@users.noreply.github.com>
2019-08-04 14:27:14 +0300

    gitignore

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
-import sanic
-import pymongo
+from sanic import Sanic
+from sanic.views import HTTPMethodView
+from sanic.response import text
 
-print(sanic.__version__)
-print(pymongo.__version__)
+app = Sanic('some_name')
+
+students = []
+
+
+@app.route("/x/<data>", methods=['POST'])
+def x(request, data):
+    students.append(data)
+    return text(data)
+
+@app.route("/x")
+def y(request):
+    s = ""
+    for a in students:
+        s += a + '\n'
+    return text(a)
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8000, debug=True)
