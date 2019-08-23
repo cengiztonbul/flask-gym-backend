5b40838bcef9268952556f4c20325a1fc526cfa8
furkanarsl <frkn__asl@windowslive.com>
2019-08-23 22:45:53 +0300

    view with user example

--- app/views/views.py
+++ app/views/views.py
@@ -1,6 +1,10 @@
-# from flask.views import
+from flask import render_template
 
 def init_view(app):
     @app.route('/', methods=['GET'])
     def index():
-        return {"test":"test"}
+        return render_template('index.html')
+
+    @app.route('/users/<int:user_id>')
+    def user_profile(user_id):
+        return render_template('index.html')
