9f05e2476e3f04822677fb292bac94bbf65fb371
furkanarsl <furkana@DESKTOP-LK8NJ1H.localdomain>
2019-08-30 12:59:52 +0300

    moved imports inside init function so it looks cleaner

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -1,16 +1,19 @@
-from .views import init_view
-from .test_profile_view import test_profile_view, test_data_routes
-from .trainer_controller import students_data, list_student_view
 
 
 def init_views(app):
+    from .views import init_view
     init_view(app)
 
     from .auth_views import init_auth_views
     init_auth_views(app)
 
+    from .test_profile_view import test_profile_view, test_data_routes
     test_profile_view(app)
     test_data_routes(app)
 
+    from .trainer_controller import students_data, list_student_view
     list_student_view(app)
     students_data(app)
+
+    from app.views.admin_controller import init_admin_routes
+    init_admin_routes(app)
