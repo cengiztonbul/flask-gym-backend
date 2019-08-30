25fed9c74f6e86912adb721e97a7b62fa1c6c1f8
furkanarsl <furkana@DESKTOP-LK8NJ1H.localdomain>
2019-08-30 12:58:01 +0300

    removed commented func and moved funcs to auth_views

--- app/views/views.py
+++ app/views/views.py
@@ -17,30 +17,3 @@ def init_view(app):
     @app.route('/users/<int:user_id>')
     def user_profile(user_id):
         return render_template('index.html')
-
-    # @app.route('/login', methods=['GET', 'POST'])
-    # def login():
-    #     form = LoginForm()
-    #     if request.method == 'GET':
-    #         return render_template('login.html', form=form)
-    #
-    #     if form.validate_on_submit():
-    #         return "test"
-    #     else:
-    #         return redirect("/login")
-
-    @app.route('/register', methods=['GET', 'POST'])
-    @login_required
-    def register():
-        print("-----")
-        print(current_user.email)
-        print("-----")
-        form = RegisterForm()
-        if form.validate_on_submit():
-            create_user(form.first_name.data, form.last_name.data, form.email.data, "user")
-            return "success"
-        return render_template('register.html', form=form)
-
-    @app.route('/logout')
-    def logout():
-        raise NotImplemented
