cb81d179b534987f39544eea9e2409d92e4cdff9
furkanarsl <furkana@DESKTOP-LK8NJ1H.localdomain>
2019-08-30 13:01:23 +0300

    logout,register moved to auth_views

--- app/views/auth_views.py
+++ app/views/auth_views.py
@@ -1,9 +1,9 @@
 from flask import redirect, render_template, request, flash
 
 from app.models.user import User
-from app.services.user_manager import initialize_user_for_login
-from app.utils.forms import LoginForm
-from flask_login import current_user, login_user, user_login_confirmed
+from app.services.user_manager import create_user
+from app.utils.forms import LoginForm, RegisterForm
+from flask_login import current_user, login_user, login_required,logout_user
 
 
 def init_auth_views(app):
@@ -15,17 +15,28 @@ def init_auth_views(app):
             return render_template("login.html", form=form)
         if form.validate_on_submit():
             u = User.objects(email=form.email.data).first()
-            login_user(user=u, remember=form.remember_me.data,force=True)
-            x = current_user
-            print(current_user.email)
-
+            login_user(user=u, remember=form.remember_me.data)
             return redirect("/")
         else:
             flash("test")
             return render_template("login.html", form=form)
-        # if user is None:
-        #     return redirect("/login")
 
-    # @app.route("/login", methods=['GET'])
-    # def login(request):
-    #     return render_template("login.html")
+
+    @app.route('/register', methods=['GET', 'POST'])
+    @login_required
+    def register():
+        print("-----")
+        print(current_user.email)
+        print("-----")
+        form = RegisterForm()
+        if form.validate_on_submit():
+            create_user(form.first_name.data, form.last_name.data, form.email.data, "user")
+            return "success"
+        return render_template('register.html', form=form)
+
+
+    @app.route("/logout")
+    @login_required
+    def logout():
+        logout_user()
+        return redirect("/")
