ac219e10b9be0b6ba9758728f8faaa45379fbf4d
cengiztonbul <cengiztonbul@outlook.com>
2019-08-30 23:43:29 +0300

    delete redundant code

--- run.py
+++ run.py
@@ -1,9 +1,4 @@
 from app import create_app
-from app.models.trainer import Trainer
-from app.models.user import User
-from app.services.database import init_db
-
-init_db()
 
 
 if __name__ == '__main__':
