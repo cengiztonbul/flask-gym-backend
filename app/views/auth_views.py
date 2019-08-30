0bc7ee74024edf9f46aa0322965a3920f84051ca
furkanarsl <furkana@DESKTOP-LK8NJ1H.localdomain>
2019-08-30 13:04:53 +0300

    removed prints

--- app/views/auth_views.py
+++ app/views/auth_views.py
@@ -3,11 +3,10 @@ from flask import redirect, render_template, request, flash
 from app.models.user import User
 from app.services.user_manager import create_user
 from app.utils.forms import LoginForm, RegisterForm
-from flask_login import current_user, login_user, login_required,logout_user
+from flask_login import current_user, login_user, login_required, logout_user
 
 
 def init_auth_views(app):
-
     @app.route("/login", methods=['GET', 'POST'])
     def login():
         form = LoginForm()
@@ -21,19 +20,14 @@ def init_auth_views(app):
             flash("test")
             return render_template("login.html", form=form)
 
-
     @app.route('/register', methods=['GET', 'POST'])
     @login_required
     def register():
-        print("-----")
-        print(current_user.email)
-        print("-----")
         form = RegisterForm()
         if form.validate_on_submit():
             create_user(form.first_name.data, form.last_name.data, form.email.data, "user")
             return "success"
         return render_template('register.html', form=form)
-
 
     @app.route("/logout")
     @login_required
--- app/views/views.py
+++ app/views/views.py
@@ -1,17 +1,13 @@
 from flask import render_template, request, redirect
 from ..utils.forms import LoginForm, RegisterForm
 from ..services.user_manager import create_user
-from flask_login import login_required, current_user
+from flask_login import login_required
 
 
 def init_view(app):
     @app.route('/', methods=['GET'])
     @app.route('/index')
     def index():
-        try:
-            print(current_user.email)
-        except Exception as e:
-            print(e)
         return render_template('index.html')
 
     @app.route('/users/<int:user_id>')
